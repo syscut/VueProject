0f55bcf6270bb33b4c08b92ea1b73da086c147be
kim <kim.hu@gfc.com.tw>
2022-05-13 17:28:35 +0800

    add router

--- basd060/src/App.vue
+++ basd060/src/App.vue
@@ -1,60 +1,44 @@
 <template>
-  <v-app>
-    <v-app-bar app color="primary" dark hide-on-scroll dense>
-      <v-app-bar-nav-icon>
-        <v-tooltip bottom>
-          <template v-slot:activator="{ on, attrs }">
-            <a v-bind="attrs" v-on="on" href="http://gfcweb/gfc">
-              <v-img
-                class="elevation-4"
-                src="./assets/gfc.gif"
-                max-height="37"
-                max-width="37"
-              />
-            </a>
-          </template>
-          <span>回首頁</span>
-        </v-tooltip>
-      </v-app-bar-nav-icon>
-
-      <v-spacer></v-spacer>
-      <v-toolbar-title> 客戶資料檔維護(basd060) </v-toolbar-title>
-      <v-spacer></v-spacer>
-
-      <v-tooltip bottom color="primary">
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn
-            href="https://github.com/vuetifyjs/vuetify/releases/latest"
-            target="_blank"
-            v-bind="attrs"
-            v-on="on"
-            text
-          >
-            <v-icon>mdi-help-circle-outline</v-icon>
-          </v-btn>
-        </template>
-        <span>程式說明</span>
-      </v-tooltip>
-    </v-app-bar>
-
-    <v-main>
-      <Basd060Main />
-    </v-main>
-  </v-app>
+  <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+  </div>
 </template>
-
 <script>
-import Basd060Main from "./components/Basd060Main";
+// import Cookies from "js-Cookies";
 
 export default {
-  name: "App",
-
-  components: {
-    Basd060Main,
+  name: "Basd060",
+  data() {
+    return { userName: "", password: "" };
   },
-
-  data: () => ({
-    //
-  }),
+  methods: {
+    logIn() {},
+  },
 };
 </script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
--- basd060/src/main.js
+++ basd060/src/main.js
@@ -1,10 +1,37 @@
 import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
+import VueRouter from "vue-router";
+import Basd060 from "@/views/Basd060.vue";
 
 Vue.config.productionTip = false;
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: Basd060,
+  },
+  {
+    path: "/about",
+    name: "about",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "@/views/AboutView.vue"),
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
 
 new Vue({
   vuetify,
+  router,
   render: (h) => h(App),
 }).$mount("#app");
